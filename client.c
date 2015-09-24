497e66b408ca303f391a505960a7c3b96d43edec
Gabriel <gabriel.crispino@hotmail.com>
2015-09-24 15:53:49 -0700

    files and code added

---
+++ client.c
@@ -0,0 +1,109 @@
+// Name of Author(s): Gabriel Nunes Crispino
+// Course Number and Name: CSE 434, Computer Networks
+// Semester: Fall 2015
+// Project Part: 1
+// Time Spent: 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <netdb.h>
+
+//function that verifies errors after some functions
+void test_err(int test,int type){
+  //if type is 0, its a read() function. Otherwise, its a write() function
+  
+  char s[20];
+  
+  if (test)
+    return;
+  else{
+    if (type)
+      strcpy(s,"write");
+    else
+      strcpy(s,"read");
+    
+    printf("Error on %s() function!\n",s);
+    exit(0);
+  } 
+  
+}
+
+
+int main(int argc, char *argv[]){
+    //argument checking
+    if ((argc - 1) != 3){
+	printf("Number of arguments must be 3!\n");
+	exit(0);
+    }  
+  
+    int client_number,port_number,cli_socket,test;
+  
+    char buffer[255];
+    
+    //structures used in the program
+    struct hostent *server;//structure used to handle the server hostname
+    struct sockaddr_in serv_addr;//structure used to handle the server's internet address
+    
+    
+    //arguments handling
+    client_number = atoi(argv[2]);
+    port_number = atoi(argv[3]);
+    
+    server = gethostbyname(argv[1]);
+    
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port_number);
+    bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,server->h_length);//copies address from "server" structure to "serv_addr" structure
+    
+    
+    cli_socket = socket(AF_INET,SOCK_STREAM,0);
+    
+    if (connect(cli_socket,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0){
+      printf("Error on connect() function!\n");
+      exit(0);
+    }
+    
+    sprintf(buffer,"%d",client_number); //sprintf converts integer variable "client_number" to a string and puts it into "buffer"
+    
+    test = write(cli_socket,buffer,strlen(buffer));//write client number to buffer
+    test_err(test,1);
+    
+    bzero(buffer,sizeof(buffer));
+    
+    /*reads client number verification result from the server(if content stored in "client_number" alredy exists, 
+    the server is going to store 0 or a positive number in the buffer. Otherwise, -1 is going to be stored) */
+    test = read(cli_socket,buffer,sizeof(buffer));
+    test_err(test,0);
+    
+    test = atoi(buffer);
+    if (test < 0){
+      //then client number sent to the server is valid
+    
+      bzero(buffer,sizeof(buffer));
+      
+      printf("Please enter the message to send to the server: ");
+      fgets(buffer,sizeof(buffer),stdin);
+      
+      test = write(cli_socket,buffer,strlen(buffer));
+      
+      test_err(test,1);
+      
+      test = read(cli_socket,buffer,sizeof(buffer));
+      
+      test_err(test,0);
+      
+      printf("Message from the server: %s",buffer);
+    }
+    else
+      printf("Invalid client number!\n");
+    
+    
+    close(cli_socket);
+    
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,184 @@
+// Name of Author: Gabriel Nunes Crispino
+// Course Number and Name: CSE 434, Computer Networks
+// Semester: Fall 2015
+// Project Part: 1
+// Time Spent: 
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <signal.h>
+
+#define TRUE 1
+#define MAXCONNECTIONS 5
+
+//functions
+int search_no(int client_number,int *connections);//search a client number in the array to check if it already exists.
+void add_no(int client_number,int *connections,int *nconnections);//adds a number to the array of client numbers.
+void remove_number(int client_number,int *connections,int *nconnections);//remove a number of the array of the client number after client connection ends.
+
+//global variables
+int *connections,nconnections = 0;//array that contains the client numbers(two client with the same number cannot be connected at the same time)
+
+
+//MISSING VERIFICATION ON THE NUMBER OF CLIENTS!!!!!!!!!!!!!
+
+int main(int argc, char *argv[]){
+    int serv_socket,cli_socket;//socket file descriptor
+    int portno;//portnumber
+    int client_len;//Contains the size of the "client_addr" structure. Used on the "accept()" function.
+    int test;//variable used to check if read() and write() function were executed with no errors.
+    int client_number;
+    int search;//result of search_no() function
+    pid_t child_proc;//variable that contains the pid number for the child processes created after the connection establishment
+    
+    char buffer[255];
+    
+    struct sockaddr_in serv_addr,client_addr;
+    
+    //check if the arguments are correct
+    if (argc != 2){
+	printf("Number of arguments has to be one!\n");
+	exit(0);
+    }
+    else
+      portno = atoi(argv[1]);
+    
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    
+    connections = mmap(0,MAXCONNECTIONS * sizeof(int),PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1,0);
+    
+    memset(connections,0,50*sizeof(int));//initialize the "connections" array with zeros
+    
+    serv_socket = socket(AF_INET,SOCK_STREAM,0);
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    
+    if (bind(serv_socket,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0){
+	printf("Error on bind() function!\n");
+	exit(0);
+    }
+    
+    if (listen(serv_socket,5) < 0){
+	printf("Error on listen() function!\n");
+	exit(0);
+    }
+    
+        
+    while(TRUE){
+      bzero(buffer,sizeof(buffer));
+      
+      client_len = sizeof(client_addr);
+      
+      printf("Waiting for client socket...\n");
+      
+      cli_socket = accept(serv_socket,(struct sockaddr *)&client_addr,&client_len);
+      
+      //if (nconnections < 5)
+      if (cli_socket == -1){
+	  printf("Error on accept() function!\n");
+	  exit(0);
+      }
+      else{
+	//reads client number from client
+	test = read(cli_socket,buffer,sizeof(buffer));
+	  
+	if (test == -1){
+	    printf("Error on read() function!\n");
+	    exit(0);
+	}
+	
+	
+	if (atoi(buffer)){
+	    //if the passed string is a number
+	    search = search_no(atoi(buffer),connections);
+	    if (search < 0){
+	      client_number = atoi(buffer);
+	      add_no(client_number,connections,&nconnections);
+	      printf("Client number: %d\n",client_number);
+	    }
+	    else
+	      printf("Invalid client number!\n");
+	    
+	    //write search variable content to buffer
+	    sprintf(buffer,"%d",search);
+	    test = write(cli_socket,buffer,strlen(buffer));
+	}
+	else{
+	  printf("Invalid client number!\n");
+	  exit(0);
+	}
+	
+	child_proc = fork();//creates child process to handle client requests
+	
+	if (!child_proc){
+	  if (search >= 0)//if client number is invalid, the child process is terminated
+	    exit(0);
+	  
+	  printf("Connection with client socket %d established!\n",client_number);
+	
+	  bzero(buffer,sizeof(buffer));
+	  
+	  test = read(cli_socket,buffer,sizeof(buffer));
+	  
+	  if (test == -1){
+	    printf("Error on read() function!\n");
+	    exit(0);
+	  }
+	  
+	  printf("Message from client %d: %s",client_number,buffer);
+	  
+	  printf("Connection with client socket %d terminated!\n",client_number);
+	  write(cli_socket,"Goodbye client\n",15);
+	  
+	  remove_number(client_number,connections,&nconnections);
+	  
+	}
+      }
+    }
+    
+    close(cli_socket);
+    close(serv_socket);
+      
+    return 0;  
+}
+
+//------------------------------------------------------------------------------
+
+int search_no(int client_number,int *connections){
+  int i;
+  for (i = 0;i < MAXCONNECTIONS;i++)
+    if (connections[i] == client_number)
+      //found "client_number"
+      return i;
+    
+    //if it doesn't find "client_number", it returns -1
+    return -1;
+}
+
+void add_no(int client_number,int *connections,int *nconnections){
+  int i;
+  
+  for (i = 0;i < MAXCONNECTIONS;i++)
+    if (!connections[i]){
+      connections[i] = client_number;
+      *(nconnections)++;
+      break;
+    }
+}
+
+void remove_number(int client_number,int *connections,int *nconnections){
+  int pos = search_no(client_number,connections,nconnections);
+  if (pos == -1)
+    *(nconnections)--;
+  connections[pos] = 0;
+}
